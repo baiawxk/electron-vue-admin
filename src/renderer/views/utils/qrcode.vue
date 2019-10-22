fc133742f34865f12c32f9efc53ad00c8f0f5b27
陈洪强 <baiawxk@qq.com>
2019-10-22 19:30:40 +0800

    update

---
+++ src/renderer/views/utils/qrcode.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <el-form>
+      <el-form-item>
+        <el-input
+          type="textarea"
+          v-model="qrcodeStr"
+        ></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button
+          type="primary"
+          @click="genQrCode"
+        >Gen</el-button>
+      </el-form-item>
+    </el-form>
+    <canvas ref="canvas"></canvas>
+
+  </div>
+</template>
+
+<script>
+import QRCode from "qrcode";
+export default {
+  name: "GenQrCodePage",
+  data() {
+    return {
+      qrcodeStr: ""
+    };
+  },
+  methods: {
+    genQrCode() {
+      let qrcodeStr = this.$data.qrcodeStr;
+      let canvas = this.$refs.canvas;
+      QRCode.toCanvas(canvas, qrcodeStr, function(error) {
+        if (error) console.error(error);
+        console.log("success!");
+      });
+    }
+  }
+};
+</script>
